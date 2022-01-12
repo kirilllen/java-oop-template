15af9a0ac223f4a979ee5f50f0b1c4556b287763
Kirill Tapkov <izhjup@mail.ru>
2022-01-12 23:49:42 +0400

    FindByName Book method

--- src/main/java/com/epam/izh/rd/online/repository/SimpleSchoolBookRepository.java
+++ src/main/java/com/epam/izh/rd/online/repository/SimpleSchoolBookRepository.java
@@ -1,17 +1,36 @@
 package com.epam.izh.rd.online.repository;
 
+import com.epam.izh.rd.online.entity.Author;
 import com.epam.izh.rd.online.entity.Book;
+import com.epam.izh.rd.online.entity.SchoolBook;
 
-public class SimpleSchoolBookRepository implements BookRepository {
+public class SimpleSchoolBookRepository implements BookRepository<SchoolBook> {
+    public final int MAX_NUMBER=1_000_000;
+    public static int schoolBookNumber=0; //счётчик количества книг
+    SchoolBook[] schoolBooks=new SchoolBook[MAX_NUMBER]; //по ТЗ нельзя использовать ArrayList (коллекции), примем максимальное количество книг 1_000_000, т.к. размер массива в ходе выполнения программы менять нельзя
 
     @Override
-    public boolean save(Book book) {
+    public boolean save(SchoolBook book) {
         return false;
     }
 
     @Override
-    public Book[] findByName(String name) {
-        return new Book[0];
+    public SchoolBook[] findByName(String name) {
+        int[] booksId=new int[MAX_NUMBER]; //переменная для возвращения автора
+        SchoolBook[] books; //массив для хранения найденных книг
+        int counter=0; //переменная-счётчик найденных книг
+        for (int i=0; i<schoolBookNumber; i++){
+            if (name.equals(schoolBooks[i].getName())) {
+                booksId[counter]=i;
+                counter++;
+
+            }
+        }
+        books=new SchoolBook[counter];
+        for (int i=0;i<counter;i++){
+            books[i]=schoolBooks[booksId[i]];
+        }
+        return books;
     }
 
     @Override
@@ -21,6 +40,6 @@ public class SimpleSchoolBookRepository implements BookRepository {
 
     @Override
     public int count() {
-        return 0;
+        return schoolBookNumber;
     }
 }
